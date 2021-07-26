84428ec5bb3de74154095b28d7455ab6a424c45a
brian3t <ngxtri@gmail.com>
2021-07-25 22:03:33 -0700

    Js mail done.

--- mail.js
+++ mail.js
@@ -1,4 +1,5 @@
 const API_ROOT = '//usv/wapi/i.php'
+const SUBJ = 'Contact Form from CraftBelly'
 
 jQuery(document).ready(($) => {
   $('#mail_btn').on('click', async (btn) => {
@@ -6,10 +7,15 @@ jQuery(document).ready(($) => {
     $(btn).prop('disabled', true)
     console.log(`btn click`, btn)
     try {
-      let mail_res = $.get(API_ROOT, {to: 'to'})
+      const now = (new Date()).toLocaleString()
+      const content = $('#cont_inp').val()
+      const to = $('#from_email').val()
+      const to_name = $('#name_inp').val()
+      let mail_res = $.get(API_ROOT, {act: 'mail', subj: `${SUBJ} ${now}`, to, to_name, cont: content})
       console.log(`Mail API response: `, mail_res)
     } catch (e) {
       console.error(`Error calling mail API `, e)
     }
+    $(btn).prop('disabled', false)
   })
 })
