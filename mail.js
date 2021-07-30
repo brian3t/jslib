c01c8e05ff6f8c9fa6a9c77625c0351af2870e97
brian3t <ngxtri@gmail.com>
2021-07-29 21:17:46 -0700

    Js mail with toastr.

--- mail.js
+++ mail.js
@@ -1,6 +1,3 @@
-const API_ROOT = '//usv/wapi/i.php'
-const SUBJ = 'Contact Form from CraftBelly'
-
 jQuery(document).ready(($) => {
   $('#mail_btn').on('click', async (btn) => {
     btn = btn.target
@@ -11,11 +8,20 @@ jQuery(document).ready(($) => {
       const content = $('#cont_inp').val()
       const to = $('#from_email').val()
       const to_name = $('#name_inp').val()
-      let mail_res = $.get(API_ROOT, {act: 'mail', subj: `${SUBJ} ${now}`, to, to_name, cont: content})
+      let mail_res = await $.get(API_ROOT, {act: 'mail', subj: `${SUBJ} ${now}`, to, to_name, cont: content})
       console.log(`Mail API response: `, mail_res)
+      if (!mail_res) {
+        console.error(`Error calling mail API `, e)
+        toastr.error(`Error sending email`)
+        return
+      }
+      if (mail_res.stat && mail_res.stat === 1){
+        toastr.success(mail_res.msg)
+      }
     } catch (e) {
       console.error(`Error calling mail API `, e)
+      toastr.error(`Error sending email`)
     }
-    $(btn).prop('disabled', false)
+    // $(btn).prop('disabled', false)
   })
 })
---
+++ mail_conf.js
@@ -0,0 +1,5 @@
+/**
+ * Mail config, used by mail.js
+ */
+const API_ROOT = '//usv/wapi/i.php'
+const SUBJ = 'Contact Form from CraftBelly'
