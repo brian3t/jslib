add648a9be14f995433fd4dfe7408dcdbdfdc875
brian3t <ngxtri@gmail.com>
2023-07-26 18:05:24 -0700

    add Using <template> to js_snippets

--- jssnippet/snippet.js
+++ jssnippet/snippet.js
@@ -4,3 +4,8 @@ window.scrollTo({
   left: 0,
   behavior: 'smooth'
 })
+
+//02 Using <template>
+let item_info_tmpl = $('#template_id').clone()
+let item_info_content = $(item_info_tmpl.prop('content'))
+$('#wrapper').append(item_info_tmpl.prop('content'))
